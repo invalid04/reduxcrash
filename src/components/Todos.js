a5c438a870e1e85c2112b5d6a4dddc490fa8928e
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-11 01:49:09 -0500

    todo page styling

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,28 +3,26 @@ import { useDispatch, useSelector } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
 export default function Todos() {
-
-
     const todos = useSelector(state => state.todos.todos)
     const dispatch = useDispatch()
 
     return (
-        <>
-            <div>
-                Todos
+        <div className="max-w-md mx-auto mt-10">
+            <h1 className="text-2xl font-bold mb-5">Todos</h1>
+            <ul className="space-y-4">
                 {todos.map((todo) => (
-                    <div>
-                        <div key={todo.id} className=''>
-                            {todo.text}
-                        </div>
+                    <li key={todo.id} className="flex justify-between items-center bg-gray-800 p-4 rounded shadow">
+                        <span className="text-white">{todo.text}</span>
                         <button
                             onClick={() => dispatch(removeTodo(todo.id))}
+                            className="text-red-500 hover:text-red-700"
                         >
-                            Remove todo
+                            Remove
                         </button>
-                    </div>
+                    </li>
                 ))}
-            </div>
-        </>
+            </ul>
+        </div>
     )
 }
+
