008ebee28358bc5473b6a0224222b1e6c5e4b022
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-11 01:50:43 -0500

    addTodo styling

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,32 +3,31 @@ import { useDispatch } from 'react-redux'
 import { addTodo } from '../features/todo/todoSlice'
 
 export default function AddTodo() {
-
     const [input, setInput] = useState('')
     const dispatch = useDispatch()
 
     const addTodoHandler = (e) => {
         e.preventDefault()
-
         dispatch(addTodo(input))
         setInput('')
     }
 
     return (
-        <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
+        <form onSubmit={addTodoHandler} className='flex items-center justify-center space-x-3 mt-12'>
             <input 
                 type='text'
-                className='bg-gray-800 rounded border border-gray-700 py-1 px-3'
-                placeholder = 'Enter a todo...'
+                className='bg-gray-800 rounded border border-gray-700 py-1 px-3 text-white focus:outline-none focus:border-indigo-500'
+                placeholder='Enter a todo...'
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             />
             <button
                 type='submit'
-                className='text-white bg-indigo-500 border-0'
+                className='text-white bg-indigo-500 py-1 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50'
             >
                 Add Todo
             </button>
         </form>
     )
 }
+
