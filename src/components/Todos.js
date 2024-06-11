771d5ce521486284575b405dea3aa2f87768ed41
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-11 01:47:51 -0500

    remove todo functionallity

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 
 export default function Todos() {
 
 
     const todos = useSelector(state => state.todos.todos)
+    const dispatch = useDispatch()
 
     return (
         <>
             <div>
                 Todos
                 {todos.map((todo) => (
-                    <div key={todo.id}>
-                        {todo.text}
+                    <div>
+                        <div key={todo.id} className=''>
+                            {todo.text}
+                        </div>
+                        <button
+                            onClick={() => dispatch(removeTodo(todo.id))}
+                        >
+                            Remove todo
+                        </button>
                     </div>
                 ))}
             </div>
