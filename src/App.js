271ae5509b768b29680ab5d82b4357194ce78d0b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-11 01:52:54 -0500

    app styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
 
-
 function App() {
   return (
-    <div className="App">
-      <AddTodo />
-      <Todos />
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-start p-5">
+      <div className="w-full max-w-md space-y-8">
+        <AddTodo />
+        <Todos />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
